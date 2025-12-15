f1837d1f8b7fe41306baf0e217c10049954d038f
Daniela Reyes <danielareyesdeveloper@gmail.com>
2025-12-15 21:32:49 +0100

    Redesign homepage with new sections and styles
    Added 'Sobre la Marca', 'Productos destacados', and 'Comentarios' sections to index.html, including new images and user icon. Updated JavaScript for fixed navigation and active menu highlighting. Enhanced CSS for new homepage layout and improved navigation bar behavior and appearance.

--- js/JavaS.js
+++ js/JavaS.js
@@ -6,11 +6,23 @@ fetch('./js/noticias.json')
         noticias.forEach(post => {
             noticiasSection.innerHTML += `
                     <article class="noticia">
-                        <h2>${post.titulo}</h2>
+                        <h3>${post.titulo}</h3>
                         <p>${post.contenido}</p>
                         <span>Publicado el: ${post.fecha}</span>
                     </article>
                     `;
         });
     });
-
+// Navegación fija al hacer scroll
+$(window).on("scroll", function () {
+    if ($(window).scrollTop() > 140) {
+        $('#nav-bar').addClass('fixed-nav');
+    } else {
+        $('#nav-bar').removeClass('fixed-nav');
+    }
+});
+//mantener activo un enlace del menú
+$('#nav-bar a').on('click', function () {
+    $('#nav-bar a').removeClass('active');
+    $(this).addClass('active');
+});
